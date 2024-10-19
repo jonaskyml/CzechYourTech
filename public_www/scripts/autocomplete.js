034d3adc1e8cb47c283d836b4cf4e3a06f9ef1ea
jonas <jonaskyml@proton.me>
2024-10-19 21:52:48 +0200

    suggestions redirect to actual pages

--- public_www/scripts/autocomplete.js
+++ public_www/scripts/autocomplete.js
@@ -1,34 +1,41 @@
 let availableKeywords = [
-    'CzechYourTech: Jak tuto webovou stránku používat?',
-    'Obsidian: Nejbezpečnější místo pro vaše poznámky',
-    'Proton: Nejspolehlivější e-mail poskytovatel a mnoho dalšího',
+    {
+        text: 'CzechYourTech: Jak tuto webovou stránku používat?',
+        url: 'blog/czechyourtech.html'
+    },
+    {
+        text: 'Obsidian: Nejbezpečnější místo pro vaše poznámky',
+        url: 'blog/obsidian.html'
+    },
+    {
+        text: 'Proton: Nejspolehlivější e-mail poskytovatel a mnoho dalšího',
+        url: 'sblog/proton.html'
+    },
 ];
 
 const resultsBox = document.querySelector(".result-box");
 const inputBox = document.getElementById("input-box");
 
-inputBox.onkeyup = function(){
+inputBox.onkeyup = function () {
     let result = [];
     let input = inputBox.value;
-    if(input.length){
-        result= availableKeywords.filter((keyword)=>{
-            return keyword.toLowerCase().includes(input.toLowerCase());
+    if (input.length) {
+        result = availableKeywords.filter((keyword) => {
+            return keyword.text.toLowerCase().includes(input.toLowerCase());
         });
-
-        console.log(result);
     }
 
     display(result);
 }
 
-function display(result){
-    const content = result.map((list)=>{
-        return "<li onclick=selectInput(this)>" + list + "</li>";
+function display(result) {
+    const content = result.map((item) => {
+        return `<li class="result-item my-2 hover:bg-black hover:bg-opacity-50 hover:shadow-lg hover:text-white p-4 cursor-pointer rounded-xl transition ease-out duration-200" onclick="selectInput('${item.url}')">${item.text}</li>`;
     });
 
     resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
 }
 
-function selectInput(list){
-    inputBox.value = list.innerHTML;
+function selectInput(url) {
+    window.location.href = url; // Redirect to the specified URL
 }
